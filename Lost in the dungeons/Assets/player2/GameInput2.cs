0b2b2d7638740432aa06141cbd8b8c717e71a99f
enukc <ananas.7@list.ru>
2025-12-05 17:18:43 +0300

    Create Gun
    Создана система стрельбы

--- Lost in the dungeons/Assets/player2/GameInput2.cs
+++ Lost in the dungeons/Assets/player2/GameInput2.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
 
 public class GameInput2 : MonoBehaviour
 {
@@ -6,17 +8,35 @@ public class GameInput2 : MonoBehaviour
 
     private PlayerActions _actions;
 
+    public event EventHandler OnPlayerAttack;
+
     private void Awake()
     {
         instance = this;
 
         _actions = new PlayerActions();
         _actions.Enable();
+
+        _actions.Combat.Attack.started += Attack_started;
+    }
+
+    private void Attack_started(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        if (OnPlayerAttack != null)
+        {
+            OnPlayerAttack.Invoke(this, EventArgs.Empty);
+        }
     }
 
     public Vector2 GetMovementVector()
     {
         Vector2 InputVector = _actions.PLayer.Move.ReadValue<Vector2>();
         return InputVector;
+    }
+
+    public Vector2 GetMousePosition()
+    {
+        Vector2 MousePosition = _actions.PLayer.MousePosition.ReadValue<Vector2>();
+        return MousePosition;
     }
 }
--- Lost in the dungeons/Assets/player2/player2.cs
+++ Lost in the dungeons/Assets/player2/player2.cs
@@ -6,14 +6,31 @@ public class Player2 : MonoBehaviour
 
     private Rigidbody2D rb;
 
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+
+    private void Start()
+    {
+        GameInput2.instance.OnPlayerAttack += GameInput_OnPlayerAttack;
+    }
+
+    private void GameInput_OnPlayerAttack(object sender, System.EventArgs e)
+    {
+        ActiveGun.Instance.GetActiveGun().Shoot();
+    }
+
     private void FixedUpdate()
     {
         Vector2 inputVector = GameInput2.instance.GetMovementVector();
         inputVector = inputVector.normalized;
         rb.MovePosition(rb.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
+
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(GameInput2.instance.GetMousePosition());
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
     }
 }
